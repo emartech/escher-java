95ce2d6f71921f7c2a326a12be4e7c12eece1e52
rgargya <rgargya@emarsys.com>
2015-02-02 17:20:27 +0100

    dataprovider introduced in test

--- src/test/java/com/emarsys/escher/HelperTest.java
+++ src/test/java/com/emarsys/escher/HelperTest.java
@@ -2,22 +2,44 @@ package com.emarsys.escher;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class HelperTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        String[] fileList = new String[] {
+                "src/test/fixtures/aws4_testsuite/get-vanilla.json"
+        };
+
+        ArrayList<Object[]> testCases = new ArrayList<>();
+        for (String fileName : fileList) {
+            testCases.add(new String[] { fileName });
+        }
+        return testCases;
+    }
+
+
+    private String fileName;
+
+
+    public HelperTest(String fileName) {
+        this.fileName = fileName;
+    }
+
 
     @Test
     public void testCanonicalize() throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
-        TestParam param = mapper.readValue(new File("src/test/fixtures/aws4_testsuite/get-vanilla.json"), TestParam.class);
+        TestParam param = mapper.readValue(new File(this.fileName), TestParam.class);
         TestParam.Request paramRequest = param.getRequest();
 
         List<String[]> headers = new ArrayList<>();
