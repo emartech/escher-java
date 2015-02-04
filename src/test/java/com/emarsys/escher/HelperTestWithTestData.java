f63eaa9fb1fd962cbf6d57e2d3c9f59b8b0b6a9e
rgargya <rgargya@emarsys.com>
2015-02-04 13:36:10 +0100

    rename test classes

---
+++ src/test/java/com/emarsys/escher/HelperTestWithTestData.java
@@ -0,0 +1,143 @@
+package com.emarsys.escher;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class HelperTestWithTestData {
+
+    private String fileName;
+    private TestParam param;
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        String[] fileList = new String[] {
+                "get-vanilla",
+                "post-vanilla",
+                "get-vanilla-query",
+                "post-vanilla-query",
+                "get-vanilla-empty-query-key",
+                "post-vanilla-empty-query-value",
+                "get-vanilla-query-order-key",
+                "post-x-www-form-urlencoded",
+                "post-x-www-form-urlencoded-parameters",
+
+                "get-header-value-trim",
+                "post-header-key-case",
+                "post-header-key-sort",
+                "post-header-value-case",
+
+                "get-vanilla-query-order-value",
+                "get-vanilla-query-order-key-case",
+                "get-unreserved",
+                "get-vanilla-query-unreserved",
+//                "get-vanilla-ut8-query",
+                "get-utf8",
+                "get-space",
+//                "post-vanilla-query-space",
+//                "post-vanilla-query-nonunreserved",
+
+//                "get-slash",
+//                "get-slashes",
+//                "get-slash-dot-slash",
+//                "get-slash-pointless-dot",
+//                "get-relative",
+//                "get-relative-relative",
+        };
+
+        ArrayList<Object[]> testCases = new ArrayList<>();
+        for (String fileName : fileList) {
+            testCases.add(new String[] { "src/test/fixtures/aws4_testsuite/" + fileName + ".json" });
+        }
+        return testCases;
+    }
+
+
+    public HelperTestWithTestData(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        param = mapper.readValue(new File(this.fileName), TestParam.class);
+    }
+
+
+    @Test
+    public void testCanonicalize() throws Exception {
+
+        TestParam.Request paramRequest = param.getRequest();
+
+        List<NameValuePair> headers = new ArrayList<>();
+        for (List<String> header : paramRequest.getHeaders()) {
+            headers.add(new BasicNameValuePair(header.get(0), header.get(1)));
+        }
+
+        URI uri = new URI("http://" + paramRequest.getHost() + paramRequest.getUrl());
+
+        Request request = new Request(paramRequest.getMethod(), uri, headers, paramRequest.getBody());
+
+        String canonicalised = Helper.canonicalize(request);
+
+        assertEquals(fileName, param.getExpected().getCanonicalizedRequest(), canonicalised);
+    }
+
+
+    @Test
+    public void testCalculateStringToSign() throws Exception {
+        String stringToSign = Helper.calculateStringToSign(param.getConfig().getCredentialScope(),
+                param.getExpected().getCanonicalizedRequest(),
+                getConfigDate(),
+                param.getConfig().getHashAlgo(),
+                param.getConfig().getAlgoPrefix());
+        assertEquals(fileName, param.getExpected().getStringToSign(), stringToSign);
+    }
+
+
+    private Date getConfigDate() throws ParseException {
+        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(param.getConfig().getDate());
+    }
+
+
+    @Test
+    public void testCalculateAuthHeader() throws Exception {
+        byte[] signingKey = Helper.calculateSigningKey(
+                param.getConfig().getApiSecret(),
+                getConfigDate(),
+                param.getConfig().getCredentialScope(),
+                param.getConfig().getHashAlgo(),
+                param.getConfig().getAlgoPrefix()
+        );
+        String authHeader = Helper.calculateAuthHeader(
+                param.getConfig().getAccessKeyId(),
+                getConfigDate(),
+                param.getConfig().getCredentialScope(),
+                signingKey,
+                param.getConfig().getHashAlgo(),
+                param.getConfig().getAlgoPrefix(),
+                param.getHeadersToSign(),
+                param.getExpected().getStringToSign()
+        );
+        assertEquals(fileName, param.getExpected().getAuthHeader(), authHeader);
+    }
+
+}
